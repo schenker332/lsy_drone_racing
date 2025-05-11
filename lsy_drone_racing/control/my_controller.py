bbf4c8d1fb177e2bece1dfe77de62c614a90f449
schenker332 <schenker.schenk@gmail.com>
2025-05-11 21:47:52 +0200

    with plottin

--- lsy_drone_racing/control/my_controller.py
+++ lsy_drone_racing/control/my_controller.py
@@ -6,7 +6,7 @@ from lsy_drone_racing.control import Controller
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
-# from scripts.plotting import plot_3d
+from scripts.plotting import plot_3d
 
 
 
@@ -153,8 +153,8 @@ class TrajectoryController(Controller):
             "obstacle_log": self._obstacle_log
         })
 
-        # plot_3d(self._saved_trajectory[-1])
 
+        plot_3d(self._saved_trajectory[-1])
 
 
 
